25b3cdcb42258a708b6beca0ebd30e2ab00fb3ca
Wojciech Koczwara <wkoczwara@sii.pl>
2022-12-09 19:01:09 +0100

    fix

--- src/test/java/aceToys/AceToysSimulation.java
+++ src/test/java/aceToys/AceToysSimulation.java
@@ -106,16 +106,26 @@ public class AceToysSimulation extends Simulation {
 //    }
 //
     {
-        if (TEST_TYPE == "INSTANT_USERS") {
-            setUp(TestPopulation.instantUsers).protocols(httpProtocol);
-        } else if(TEST_TYPE == "COMPLEX_INJECTION") {
+        if (TEST_TYPE.equals("INSTANT_USERS")) {
+            setUp(TestPopulation.instantUsers).protocols(httpProtocol)
+                    .assertions(
+                            global().responseTime().mean().lt(3),
+                            global().successfulRequests().percent().gt(99.0)
+                            , forAll().responseTime().max().lt(5)
+                    );
+        } else if(TEST_TYPE.equals("COMPLEX_INJECTION")) {
             setUp(TestPopulation.complexInjection).protocols(httpProtocol);
-        } else if (TEST_TYPE == "RAMP_USERS") {
+        } else if (TEST_TYPE.equals("RAMP_USERS")) {
         setUp(TestPopulation.rampUsers).protocols(httpProtocol);
-        } else if (TEST_TYPE == "CLOSED_MODEL") {
+        } else if (TEST_TYPE.equals("CLOSED_MODEL")) {
         setUp(TestPopulation.closedModelInjection).protocols(httpProtocol);
         } else {
-            setUp(TestPopulation.instantUsers).protocols(httpProtocol);
+            setUp(TestPopulation.instantUsers).protocols(httpProtocol)
+                    .assertions(
+                    global().responseTime().mean().lt(3),
+                    global().successfulRequests().percent().gt(99.0)
+                    , forAll().responseTime().max().lt(5)
+            );
         }
 
     }
